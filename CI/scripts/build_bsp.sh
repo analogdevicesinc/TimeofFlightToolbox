d3df031c413ef30da664bb7d45cdf06c5cfd672f
Travis F. Collins <travis.collins@analog.com>
2020-03-05 17:52:09 -0800

    Add more build tools
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/scripts/build_bsp.sh
+++ CI/scripts/build_bsp.sh
@@ -5,14 +5,13 @@ scriptdir=`dirname "$BASH_SOURCE"`
 cd $scriptdir
 cd ..
 
-apt install cmake
-
 cd /tmp/
 git clone --branch v0.3.5 --depth 1 https://github.com/google/glog
 cd glog
 mkdir build_0_3_5 && cd build_0_3_5
 cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX=/opt/glog ..
 cmake --build . --target install
+ldconfig
 
 cd /tmp/
 apt-get install libssl-dev
@@ -21,6 +20,7 @@ cd libwebsockets
 mkdir build_3_1 && cd build_3_1
 cmake -DLWS_STATIC_PIC=ON -DCMAKE_INSTALL_PREFIX=/opt/websockets ..
 cmake --build . --target install
+ldconfig
 
 cd /tmp/
 git clone --branch v3.9.0 --depth 1 https://github.com/protocolbuffers/protobuf
@@ -28,11 +28,12 @@ cd protobuf
 mkdir build_3_9_0 && cd build_3_9_0
 cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=/opt/protobuf ../cmake
 cmake --build . --target install
+ldconfig
 
 cd /tmp/
 git clone https://github.com/analogdevicesinc/aditof_sdk
 cd aditof_sdk
-git checkout v1.2.0
+git checkout "$TOFBRANCH"
 mkdir build && cd build
 cmake -DWITH_EXAMPLES=off -DWITH_MATLAB=on -DMatlab_ROOT_DIR=/usr/local/MATLAB/"$MLRELEASE" -DCMAKE_PREFIX_PATH="/opt/glog;/opt/protobuf;/opt/websockets" ..
 make
